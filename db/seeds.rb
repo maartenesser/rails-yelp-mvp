d057e8711bff35aca15ac29fbdbff5966254d690
Maarten Esser <maartenjr.esser@gmail.com>
2019-05-15 16:53:49 +0200

    finsihed excersice

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.delete_all
+
+Restaurant.create!(
+  [{
+    name: "Indian",
+    address: "Rudiditschke strasse 56",
+    phone_number: "0646017949",
+    category: 'chinese'
+  },{
+    name: "Chineese",
+    address: "Karlmarkstrasse 45",
+    phone_number: "027720204",
+    category: 'chinese'
+  }])
+
+# Review.delete_all
+
+# Review.create!(
+#   [{
+#     content: "Was a good restaurant only everything was very spicy",
+#     rating: 3,
+#     restaurant_id: @restaurant
+# }])
+
+#create aswell a review but that we'll do later.
+
+#index new show create
+#new create
